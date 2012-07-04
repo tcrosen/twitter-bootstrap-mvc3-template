a02d44cf65a82c1713023d783b6d7233c35b2490
Terry Rosen <tcrosen@gmail.com>
2012-07-04 12:24:34 -0400

    cleanup

--- Intranet/Properties/AssemblyInfo.cs
+++ Intranet/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Intranet")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("LCBO")]
+[assembly: AssemblyCompany("Terry Rosen")]
 [assembly: AssemblyProduct("Intranet")]
-[assembly: AssemblyCopyright("Copyright © LCBO 2012")]
+[assembly: AssemblyCopyright("Copyright © Terry Rosen 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.2")]
+[assembly: AssemblyFileVersion("1.0.2")]
